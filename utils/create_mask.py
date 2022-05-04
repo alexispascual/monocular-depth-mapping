1643f034e6c038b1fce7a288c52223d0bffe205d
alexispascual <ap.alexispascual@gmail.com>
2022-05-04 14:05:37 -0400

    Adding escape key back

--- utils/create_mask.py
+++ utils/create_mask.py
@@ -53,7 +53,7 @@ def draw_mask(edges, horizon_directory: str, directory: str):
         elif key == ord('f'):
             image = fill_horizon_line(image)
 
-        elif key == ord('q'):
+        elif key == ord('q') or key == 27:
             break
 
         elif key == ord('s'):
