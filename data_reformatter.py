94703df5073286cc52b74c8fb8f8ad7fcb8a8ccf
alexispascual <ap.alexispascual@gmail.com>
2022-07-20 12:12:34 -0400

    Added reformatter for campaign_1

--- data_reformatter.py
+++ data_reformatter.py
@@ -1,11 +1,26 @@
 import os
 import shutil
-from pprint import pp
 import re
+
 from tqdm import tqdm
 
+from utils.tools import check_directory
 
-def main():
+
+def format_campaign_1():
+    root_folder = 'D:/PhD/Depth Estimation/Data/campaign_1/data'
+    dest_folder = 'D:/PhD/Depth Estimation/Data/campaign_1/images'
+
+    check_directory(dest_folder)
+
+    for root, dirs, files in os.walk(root_folder):
+        for directory in dirs:
+            image_file = os.path.join(root_folder, directory, f'zed_image_left_{directory}.jpg')
+
+            shutil.copyfile(image_file, os.path.join(dest_folder, f'zed_image_left_{directory}.jpg'))
+
+
+def format_campaign_2():
     root_folder = 'D:/PhD/Depth Estimation/Analogue/analogue_july12/data'
     depth_folder = 'D:/PhD/Depth Estimation/Analogue/analogue_july12/zed-depth'
     image_folder = 'D:/PhD/Depth Estimation/Analogue/analogue_july12/zed-left'
@@ -19,9 +34,6 @@ def main():
     for root, dirs, files in os.walk(image_folder):
         image_files = [f for f in files]
 
-    pp(depth_files)
-    pp(image_files)
-
     for depth, image in tqdm(zip(depth_files, image_files)):
         name = ''.join(re.split(r'(\d+)', depth)[1:-1])
         path = os.path.join(root_folder, name)
@@ -32,4 +44,5 @@ def main():
         
 
 if __name__ == '__main__':
-    main()
+    format_campaign_1()
+    print("========= Done! =========")
